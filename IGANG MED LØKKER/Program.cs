f0adc22cf7186175194a92a5835044426b77fd07
Rasmus <Rasmus@vinperlen.dk>
2021-01-25 19:43:42 +0100

    Add project files.

---
+++ IGANG MED LØKKER/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGANG_MED_LØKKER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //###### OPGAVE A ######
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //###### OPGAVE B ######
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    if (i < 50)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //###### OPGAVE C ######
+            //int i = 0;
+            //while (i < 50)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //###### OPGAVE D ######
+            //for (int i = 100; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+        }
+    }
+}
